19c825f44acbf51649bd7a0ee7ad305fb6de3b5c
ShleeRe <konradbralewski1111@gmail.com>
2022-12-28 21:41:09 +0100

    rebase without untracked files

---
+++ IdunnoAPI/Controllers/PostsController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IdunnoAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PostsController : ControllerBase
+    {
+        [HttpGet]
+        public string Get()
+        {
+            return "get method";
+        }
+
+        [Route("Add")]
+        [HttpPost]
+        public void Add(int opId, string postDescription, string imagePath)
+        {
+
+        }
+    }
+}
---
+++ IdunnoAPI/Models/Post.cs
@@ -0,0 +1,14 @@
+﻿namespace IdunnoAPI.Models
+{
+    public class Post
+    {
+        public int OPId { get; set; } = 0;
+        public string OPName { get; set; }
+        public string OPSurname { get; set; } 
+        public string Date { get; set; }
+        public string PostDescription { get; set; }
+
+        public string ImagePath { get; set; }
+
+    }
+}
